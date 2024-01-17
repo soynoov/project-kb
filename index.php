d6d93c1a038fbd038ea09075ec84ec0992e02d57
Soy_NooV <heroyi3000@gmail.com>
2024-01-17 15:47:25 +0100

    Update

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Proyecto Kebab</title>
+</head>
+
+<body>
+    <form action="" method="post">
+        <input type="text" name="" placeholder="Usuario">
+        <input type="password" name="" placeholder="Contraseña">
+    </form>
+</body>
+
+</html>
