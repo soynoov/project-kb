ef6580bc83ead164d88390a72cf4e8cfd60436c2
Soy_NooV <heroyi3000@gmail.com>
2024-01-24 20:08:02 +0100

    Favicon

--- pages/register.php
+++ pages/register.php
@@ -38,6 +38,7 @@ try {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../style.css">
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
     <title>| Kebab</title>
 </head>
 
